2461842496a22cbb14e3110fd7e428ac663344ae
Daniel Rodríguez <sadasant@gmail.com>
2018-06-24 21:24:19 +0000

    placeholder for the verify endpoint

--- handlers.js
+++ handlers.js
@@ -3,3 +3,4 @@ import 'source-map-support/register'
 export const hello = require('./handlers/hello').default
 export const register = require('./handlers/register').default
 export const unregister = require('./handlers/unregister').default
+export const verify = require('./handlers/verify').default
