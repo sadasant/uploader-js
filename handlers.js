9ce09941af7ff9e557687b01a2f87632d3812302
Daniel Rodríguez <sadasant@gmail.com>
2018-07-01 11:49:20 +0000

    fmt

--- handlers.js
+++ handlers.js
@@ -1,16 +1,21 @@
 import 'babel-polyfill'
 import 'source-map-support/register'
 
-export default Object.assign({}, 
-        ['register', 
-        'unregister', 
-        'removeAcount', 
-        'verify', 
-        'listUploads', 
-        'shareUpload', 
-        'upload', 
-        'removeUpload', 
-        'authorize', 
-        'getAuthToken', 
-        'getVerifyToken']
-    .map((handlerName) => ({[handlerName]: require('./handlers/'+handlerName).default}) ))
+export default Object.assign(
+  {},
+  [
+    'register',
+    'unregister',
+    'removeAcount',
+    'verify',
+    'listUploads',
+    'shareUpload',
+    'upload',
+    'removeUpload',
+    'authorize',
+    'getAuthToken',
+    'getVerifyToken'
+  ].map(handlerName => ({
+    [handlerName]: require('./handlers/' + handlerName).default
+  }))
+)
